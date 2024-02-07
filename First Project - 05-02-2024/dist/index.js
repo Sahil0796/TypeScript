133b5acc63140a6c2083d96369d7bc93bcc569a0
Sahil0796 <closedsafe@gmail.com>
2024-02-07 14:25:28 +0530

    Updated

---
+++ First Project - 05-02-2024/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let age = 27;
+if (age < 50)
+    age += 10;
+//# sourceMappingURL=index.js.map
---
+++ First Project - 05-02-2024/src/index.ts
@@ -0,0 +1,11 @@
+// console.log("This is TypeScript !");
+
+export { }                // This line of code tells TypeScript that these line of codes are available in a seprate module.
+
+let age: number = 27;
+
+if (age < 50)
+    age += 10;
+
+
+
