0430d6d742c8429f4a8ed2c30e217fb5cf44ccf9
Sahil0796 <closedsafe@gmail.com>
2024-02-07 15:04:06 +0530

    Updated

--- First Project - 05-02-2024/dist/index.js
+++ First Project - 05-02-2024/dist/index.js
@@ -3,4 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 let age = 27;
 if (age < 50)
     age += 10;
+console.log(age);
 //# sourceMappingURL=index.js.map
--- First Project - 05-02-2024/src/index.ts
+++ First Project - 05-02-2024/src/index.ts
@@ -2,10 +2,27 @@
 
 export { }                // This line of code tells TypeScript that these line of codes are available in a seprate module.
 
-let age: number = 27;
+// let age: number = 27;
 
-if (age < 50)
-    age += 10;
+// if (age < 50)
+//     age += 10;
+
+//     console.log(age);
+
+let number = 123_456_789;
+let course = "TypeScript !";
+let is_published  = true;
+let level;                   // Ts assumes that this variable is of type "any".
+
+
+// Any type.
+
+
+
+
+
+
+
 
 
 
